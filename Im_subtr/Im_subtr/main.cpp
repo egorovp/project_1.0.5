6a6f0e7af3e95dd370e400761bb2107241fb4ebf
egorovp <40826999+egorovp@users.noreply.github.com>
2019-03-02 17:32:44 +0300

    Add files via upload

--- Im_subtr/Im_subtr/main.cpp
+++ Im_subtr/Im_subtr/main.cpp
@@ -29,6 +29,44 @@
 
 using namespace std;
 using namespace cv;
+
+namespace testHog
+{
+	class Detector
+	{
+		enum Mode { Default, Daimler } m;
+		HOGDescriptor hog, hog_d;
+	public:
+		Detector() : m(Daimler), hog(), hog_d(Size(48, 96), Size(16, 16), Size(8, 8), Size(8, 8), 9)
+		{
+			hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+			hog_d.setSVMDetector(HOGDescriptor::getDaimlerPeopleDetector());
+		}
+		void toggleMode() { m = (m == Default ? Daimler : Default); }
+		string modeName() const { return (m == Default ? "Default" : "Daimler"); }
+		vector<Rect> detect(InputArray img)
+		{
+			// Run the detector with default parameters. to get a higher hit-rate
+			// (and more false alarms, respectively), decrease the hitThreshold and
+			// groupThreshold (set groupThreshold to 0 to turn off the grouping completely).
+			vector<Rect> found;
+			if (m == Default)
+				hog.detectMultiScale(img, found, 0, Size(8, 8), Size(32, 32), 1.05, 2, false);
+			else if (m == Daimler)
+				hog_d.detectMultiScale(img, found, 0.5, Size(8, 8), Size(32, 32), 1.05, 2, true);
+			return found;
+		}
+		void adjustRect(Rect & r) const
+		{
+			// The HOG detector returns slightly larger rectangles than the real objects,
+			// so we slightly shrink the rectangles to get a nicer output.
+			r.x += cvRound(r.width*0.1);
+			r.width = cvRound(r.width*0.8);
+			r.y += cvRound(r.height*0.07);
+			r.height = cvRound(r.height*0.8);
+		}
+	};
+}
 /*
 struct desription
 {
@@ -99,6 +137,8 @@ int main(int argc, char** argv) {
 	
 	// \todo delete this flag
 	int flag = 0;
+	
+	testHog::Detector detector;
 	
 	while (capVideo.isOpened() && chCheckForEscKey != 27) {
 
@@ -203,7 +243,24 @@ int main(int argc, char** argv) {
 		}*/
 		
 		if (countFrames >= 20)
-			recognition(imgFrame2Copy, imgFrame1Copy, imgBack, objects, init, ELSE);
+		{
+			cv::Mat diff_hog = imgBack.clone();
+			cv:absdiff(imgBack, imgFrame2Copy, diff_hog);
+			vector<Rect> found = detector.detect(diff_hog);
+			cv::Mat frame = imgFrame2.clone();
+			if (!found.empty())
+			{
+				for (vector<Rect>::iterator i = found.begin(); i != found.end(); ++i)
+				{
+					Rect &r = *i;
+					detector.adjustRect(r);
+					rectangle(frame, r.tl(), r.br(), cv::Scalar(0, 255, 0), 2);
+				}
+				imshow("People detector", frame);
+			}
+			//recognition(imgFrame2Copy, imgFrame1Copy, imgBack, objects, init, ELSE);
+		}
+
 
 		printf("\nobjects  -  %d\n init  -  %d\n", objects.size(), init);
 
